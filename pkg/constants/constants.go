089cd31014dac77702999f719301fe830cb2e89c
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2025-03-07 17:07:01 +0530

    Make Imagename a variable to accomodate arch info

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -1,7 +1,15 @@
 package constants
 
+import (
+	"fmt"
+	"runtime"
+)
+
 const (
-	ImageName     = "quay.io/praveenkumar/microshift-okd:4.18.0-okd-scos.1-amd64"
 	ContainerName = "microshift"
 	HostName      = "127.0.0.1.nip.io"
+)
+
+var (
+	ImageName = fmt.Sprintf("quay.io/praveenkumar/microshift-okd:4.18.0-okd-scos.1-%s", runtime.GOARCH)
 )
