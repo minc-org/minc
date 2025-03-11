b35207e60d04722ef826a823ed7c83f039cbb96a
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2025-03-11 20:27:29 +0530

    cut v0.0.3

--- cmd/main.go
+++ cmd/main.go
@@ -52,7 +52,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Show the version of minc",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("version: 0.0.2")
+		fmt.Println("version: 0.0.3")
 	},
 }
 
