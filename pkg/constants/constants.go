a4a2147d3f7dfcaaa1923d0699142f71ca8b3437
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2025-09-04 12:21:53 +0530

    Update image to 4.19.0-okd-scos.17
    Signed-off-by: Praveen Kumar <kumarpraveen.nitdgp@gmail.com>

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -9,7 +9,7 @@ const (
 	ContainerName = "microshift"
 	HostName      = "127.0.0.1.nip.io"
 	LabelKey      = "io.x-openshift.microshift.cluster"
-	UShiftVersion = "4.19.0-okd-scos.7"
+	UShiftVersion = "4.19.0-okd-scos.17"
 	Registry      = "quay.io"
 	RegistryOrg   = "minc-org"
 	ImageName     = "minc"
